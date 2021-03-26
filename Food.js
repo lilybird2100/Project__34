02a07c92798eee4a1772309907b451b5b836c872
lilybird2100 <71856817+lilybird2100@users.noreply.github.com>
2021-03-26 18:24:35 -0400

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,20 @@
+class Milks {
+    constructor(){
+        var foodStock, lastFed;
+    
+        this.milk = loadImage('images/Milk.png'); 
+    }
+    
+
+    getFoodStock(){
+    }
+
+    updateFoodStock(){
+    }
+
+    deductFoodStock(){
+    }
+
+    display(x, y){      
+           }
+}
